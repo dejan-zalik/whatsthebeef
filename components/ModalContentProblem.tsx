9ede5c18b7a7fb731a5c7d9874c9962651cb9c27
Dejan <dkilaz@gmail.com>
2024-11-04 19:09:21 +0100

    updateVote loading

--- components/ModalContentProblem.tsx
+++ components/ModalContentProblem.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import { ThumbsDown, ThumbsUp } from 'lucide-react';
+import { ThumbsDown, ThumbsUp, LoaderCircle } from 'lucide-react';
 import type { ProblemProps } from '@/components/ProblemsList';
 import { useRouter } from 'next/navigation';
 import updateProblemVote from '@/app/actions/updateProblemVote';
+import { useState } from 'react';
 
 const ModalContentProblem = ({ problem }: { problem: ProblemProps }) => {
+  const [isLoadingUp, setIsLoadingUp] = useState(false);
+  const [isLoadingDown, setIsLoadingDown] = useState(false);
   const router = useRouter();
 
   const handleDownvote = async () => {
     problem.upvotes -= 1;
+    setIsLoadingDown(true);
     await updateProblemVote(problem);
     router.back();
     setTimeout(() => {
@@ -19,6 +23,7 @@ const ModalContentProblem = ({ problem }: { problem: ProblemProps }) => {
 
   const handleUpvote = async () => {
     problem.upvotes += 1;
+    setIsLoadingUp(true);
     await updateProblemVote(problem);
     router.back();
     setTimeout(() => {
@@ -30,24 +35,37 @@ const ModalContentProblem = ({ problem }: { problem: ProblemProps }) => {
     <>
       <div className="flex justify-center my-6">{problem?.description}</div>
       <div className="flex justify-center my-6">
-        <button
-          className="btn btn-ghost btn-circle shadow-md mx-3 text-green-500"
-          onClick={(e) => {
-            e.preventDefault();
-            handleUpvote();
-          }}
-        >
-          <ThumbsUp />
-        </button>
-        <button
-          className="btn btn-ghost btn-circle shadow-md mx-3 text-red-500"
-          onClick={(e) => {
-            e.preventDefault();
-            handleDownvote();
-          }}
-        >
-          <ThumbsDown />
-        </button>
+        {isLoadingUp ? (
+          <button className="btn btn-ghost btn-circle shadow-md mx-3 text-green-500">
+            <LoaderCircle className="animate-spin" />
+          </button>
+        ) : (
+          <button
+            className="btn btn-ghost btn-circle shadow-md mx-3 text-green-500"
+            onClick={(e) => {
+              e.preventDefault();
+              handleUpvote();
+            }}
+          >
+            <ThumbsUp />
+          </button>
+        )}
+
+        {isLoadingDown ? (
+          <button className="btn btn-ghost btn-circle shadow-md mx-3 text-red-500">
+            <LoaderCircle className="animate-spin" />
+          </button>
+        ) : (
+          <button
+            className="btn btn-ghost btn-circle shadow-md mx-3 text-red-500"
+            onClick={(e) => {
+              e.preventDefault();
+              handleDownvote();
+            }}
+          >
+            <ThumbsDown />
+          </button>
+        )}
       </div>
     </>
   );
